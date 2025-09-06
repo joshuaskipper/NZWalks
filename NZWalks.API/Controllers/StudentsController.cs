e4052569119de30ff7b439f02b4c83929a9d50ae
Joshua Skipper <yasoiskipper1@outlook.com>
2025-09-06 12:39:53 -0400

    Add Entity Framework Core support and initial models
    Updated project to include Entity Framework Core packages for SQL Server.
    Configured database connection in `appsettings.json` and added `DbContext` in `Program.cs`.
    Introduced `StudentsController` for student retrieval.
    Created initial migration for `Difficulties`, `Regions`, and `Walks` tables, along with domain model classes.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:7161/api/students
+    // https://localhost:7161/scalar/v1
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAllStudents() 
+        {
+            List<string> StudentNames = new List<string> 
+            {
+                "John",
+                "Bill",
+                "Shawn",
+                "Jane"
+            };
+
+            return Ok(StudentNames);
+        }
+
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 
 var app = builder.Build();
 
